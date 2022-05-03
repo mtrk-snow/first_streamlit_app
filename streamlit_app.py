c1e9745b78e64a949440ecd3a9074e58b82726d6
mtrk-snow <104834741+mtrk-snow@users.noreply.github.com>
2022-05-03 13:42:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-
-import streamlit
-
-streamlit.title("my parents new healthy diner")
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
