38ef06f8041547ef5de9462ac55d4436082ebfe8
mtrk-snow <104834741+mtrk-snow@users.noreply.github.com>
2022-05-04 09:41:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.text(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
