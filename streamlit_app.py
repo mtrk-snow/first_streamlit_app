b490d8d5f0a4d0c1e9bb896097e04a3b287664f1
mtrk-snow <104834741+mtrk-snow@users.noreply.github.com>
2022-05-04 13:46:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,18 @@ fruits_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.
 
 
 streamlit.header("Fruityvice Fruit Advice")
-
-fruit_choice = streamlit.text_input("What fruit would you like information about?", "kiwi")
-streamlit.write("The user entered", fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input("What fruit would you like information about?")#
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information about.")
+  else:
+    #streamlit.write("The user entered", fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 streamlit.stop()
 
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
