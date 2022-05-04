d1a423864d667bf1a9539bf51dec9d9e307419f9
mtrk-snow <104834741+mtrk-snow@users.noreply.github.com>
2022-05-04 13:38:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,11 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
-fruits_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index), ["Avocado","Strawberries"])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
+
+
+fruits_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index), ["Avocado","Strawberries"])
+
 
 streamlit.header("Fruityvice Fruit Advice")
 
@@ -25,6 +26,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input("What fruit would you like to add?", "jackfruit")
+streamlit.write("Thanks for adding", fruit_choice)
 
 import snowflake.connector
 
